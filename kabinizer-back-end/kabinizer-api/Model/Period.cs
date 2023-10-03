2c6359e7f9aa6ba99330f6cb8bd1b4ca935d3b40
Kamilla Nyborg <kamnyborg@gmail.com>
2023-10-03 21:30:35 +0200

    Expose special period

--- kabinizer-back-end/kabinizer-api/Model/Period.cs
+++ kabinizer-back-end/kabinizer-api/Model/Period.cs
@@ -2,10 +2,10 @@
 
 namespace kabinizer_api.Model;
 
-public record Period(DateTime PeriodStart, DateTime PeriodEnd, DateTime DeadlineDate)
+public record Period(DateTime PeriodStart, DateTime PeriodEnd, DateTime DeadlineDate, bool IsSpecialPeriod)
 {
     public static Period FromObject(PeriodEntity o)
     {
-        return new Period(o.PeriodStart, o.PeriodEnd, o.DeadlineDate);
+        return new Period(o.PeriodStart, o.PeriodEnd, o.DeadlineDate, o.IsSpecialPeriod);
     }
 }
