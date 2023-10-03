6f803aae62748f0c10fa225143f3c56abab3d3c9
wigsnes <fredrik.wigsnes@gmail.com>
2023-10-03 19:18:44 +0200

    tailwind init

--- kabinizer-front-end/src/App.tsx
+++ kabinizer-front-end/src/App.tsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import Admin from "./pages/admin";
+import Home from "./pages/home";
 
 function App() {
+  const showHome = window.location.pathname === "/";
+  const showAdmin = window.location.pathname === "/admin";
+
   return (
     <div className="app">
-      <div className="logo">Kabinizer</div>
+      <div className="text-3xl font-bold underline">Kabinizer</div>
+      <div>
+        {showHome && <Home />}
+        {showAdmin && <Admin />}
+      </div>
     </div>
   );
 }
