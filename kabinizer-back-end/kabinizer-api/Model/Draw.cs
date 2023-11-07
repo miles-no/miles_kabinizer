0f85f3c3e43668ae69c71610689dd68bd52baa6b
Marius Sørensen <marius.sorensen@miles.no>
2023-11-07 19:37:45 +0100

    Move IsSpecial to Draw

--- kabinizer-back-end/kabinizer-api/Model/Draw.cs
+++ kabinizer-back-end/kabinizer-api/Model/Draw.cs
@@ -2,10 +2,10 @@
 
 namespace kabinizer_api;
 
-public record Draw(Guid Id, DateTime Start, DateTime End, string Title)
+public record Draw(Guid Id, DateTime Start, DateTime End, string Title, bool IsSpecial = false)
 {
     public static Draw FromObject(DrawEntity o)
     {
-        return new Draw(o.Id, o.Start, o.End, o.Title);
+        return new Draw(o.Id, o.Start, o.End, o.Title, o.IsSpecial);
     }
 }
--- kabinizer-back-end/kabinizer-data/Entities/DrawEntity.cs
+++ kabinizer-back-end/kabinizer-data/Entities/DrawEntity.cs
@@ -10,5 +10,6 @@ public class DrawEntity
     public DateTime Start { get; set; }
     public DateTime End { get; set; }
     public required string Title { get; set; }
+    public bool IsSpecial { get; set; }
     public List<PeriodEntity>? Periods { get; set; }
 }
