c8a47fded6b3723e7fe5096195c2ee09a0953465
Marius Sørensen <marius.sorensen@miles.no>
2023-11-07 18:40:47 +0100

    Add Draws

---
+++ kabinizer-back-end/kabinizer-api/Model/Draw.cs
@@ -0,0 +1,11 @@
+﻿using kabinizer_data;
+
+namespace kabinizer_api;
+
+public record Draw(Guid Id, DateTime Start, DateTime End, string Title)
+{
+    public static Draw FromObject(DrawEntity o)
+    {
+        return new Draw(o.Id, o.Start, o.End, o.Title);
+    }
+}
---
+++ kabinizer-back-end/kabinizer-data/Migrations/20231107171045_CreatedDraws.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace kabinizer_data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedDraws : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "DrawId",
+                table: "Period",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "Draws",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Start = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    End = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Draws", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Period_DrawId",
+                table: "Period",
+                column: "DrawId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Period_Draws_DrawId",
+                table: "Period",
+                column: "DrawId",
+                principalTable: "Draws",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Period_Draws_DrawId",
+                table: "Period");
+
+            migrationBuilder.DropTable(
+                name: "Draws");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Period_DrawId",
+                table: "Period");
+
+            migrationBuilder.DropColumn(
+                name: "DrawId",
+                table: "Period");
+        }
+    }
+}
