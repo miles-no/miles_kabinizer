42b53921061d9833f54ad2b1d2e903e7273894ee
Kamilla Nyborg <kamnyborg@gmail.com>
2023-12-11 21:07:07 +0100

    Formatting

--- kabinizer-back-end/kabinizer-api/Model/Period.cs
+++ kabinizer-back-end/kabinizer-api/Model/Period.cs
@@ -1,27 +1,26 @@
 ﻿using kabinizer_data.Entities;
 
-namespace kabinizer_api.Model
+namespace kabinizer_api.Model;
+
+public record Period(Guid Id, DateTime PeriodStart, DateTime PeriodEnd, string Title, Guid DrawId)
 {
-    public record Period(Guid Id, DateTime PeriodStart, DateTime PeriodEnd, string Title, Guid DrawId)
+    public Period(DateTime periodStart, DateTime periodEnd, string title, Guid DrawId) 
+        : this(Guid.NewGuid(), periodStart, periodEnd, title, DrawId)
     {
-        public Period(DateTime periodStart, DateTime periodEnd, string title, Guid DrawId) 
-            : this(Guid.NewGuid(), periodStart, periodEnd, title, DrawId)
-        {
-        }
+    }
 
-        public static Period FromObject(PeriodEntity o)
-        {
-            return new Period(o.Id, o.PeriodStart, o.PeriodEnd, o.Title, o.DrawId);
-        }
+    public static Period FromObject(PeriodEntity o)
+    {
+        return new Period(o.Id, o.PeriodStart, o.PeriodEnd, o.Title, o.DrawId);
+    }
 
-        public PeriodEntity ToObject() {
-            return new PeriodEntity {
-                Id = Id,
-                PeriodStart = PeriodStart,
-                PeriodEnd = PeriodEnd,
-                Title = Title,
-                DrawId = DrawId
-            };
-        }
+    public PeriodEntity ToObject() {
+        return new PeriodEntity {
+            Id = Id,
+            PeriodStart = PeriodStart,
+            PeriodEnd = PeriodEnd,
+            Title = Title,
+            DrawId = DrawId
+        };
     }
 }
