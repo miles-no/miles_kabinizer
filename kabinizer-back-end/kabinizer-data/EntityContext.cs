ce078581020e0ee6b4ed554df0313109dcd5d1fd
Henry Skorpe Sjøen <henry.sjoen@miles.no>
2024-04-16 13:06:50 +0200

    Drop files from .gitignore (#99)
    Add `.idea` folder to the `.gitignore` file and delete all files listed in the `.gitignore` from the repository.

--- kabinizer-back-end/kabinizer-data/EntityContext.cs
+++ kabinizer-back-end/kabinizer-data/EntityContext.cs
@@ -3,13 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace kabinizer_data;
 
-public class EntityContext : DbContext
+public class EntityContext(DbContextOptions<EntityContext> options) : DbContext(options)
 {
-    public EntityContext(DbContextOptions<EntityContext> options)
-        : base(options)
-    {
-    }
-
     public DbSet<BookingRequestEntity> BookingRequests { get; set; }
     public DbSet<PeriodEntity> Periods { get; set; }
     public DbSet<DrawEntity> Draws { get; set; }
