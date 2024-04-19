8f9f96099d83c6ef5cf48368758f4b214adcf2f2
Kristoffer Marthinsen <kristoffermarthinsen@gmail.com>
2024-04-19 13:37:01 +0200

    feat(admin-ui): make title required

--- kabinizer-back-end/kabinizer-api/Dtos/Draw/UpdatePeriod.cs
+++ kabinizer-back-end/kabinizer-api/Dtos/Draw/UpdatePeriod.cs
@@ -5,7 +5,7 @@ public class UpdatePeriod {
     public required string Id { get; set; }
     public DateTime PeriodStart { get; set; }
     public DateTime PeriodEnd { get; set; }
-    public string? Title { get; set; }
+    public  required string Title { get; set; }
 
     public required string DrawId { get; set; }
 }
