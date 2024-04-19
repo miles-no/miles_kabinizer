610e74a0ffc03a69196f4a8c9a0d9bf7d312e5ac
Henry Sjøen <henry.sjoen@miles.no>
2024-04-19 20:38:01 +0200

    refactor: remove abstractions and add loading status

--- kabinizer-front-end/src/components/OptionButton.tsx
+++ kabinizer-front-end/src/components/OptionButton.tsx
@@ -1,5 +1,5 @@
-import { ColorType } from "../types";
-import { NumberOfDays } from "../utils";
+import { ColorType } from "@/types";
+import { NumberOfDays } from "@/utils";
 
 const OptionButton = ({
   colors,
@@ -8,6 +8,7 @@ const OptionButton = ({
   isSpecial,
   selected,
   onClick,
+  isLoading = false,
 }: {
   colors: ColorType;
   from: Date;
@@ -15,6 +16,7 @@ const OptionButton = ({
   isSpecial: boolean;
   selected: boolean;
   onClick: () => void;
+  isLoading?: boolean;
 }) => {
   const days = NumberOfDays(from, to) + 1;
 
@@ -27,36 +29,45 @@ const OptionButton = ({
   return (
     <button
       onClick={onClick}
-      className={`relative z-10 flex w-full flex-1 items-center justify-center rounded-full`}
+      disabled={isLoading}
+      className={`relative z-10 flex w-full flex-1 items-center justify-center rounded-full ${isLoading ? "opacity-50" : ""}`}
       style={{
         backgroundColor,
         height: "30px",
         flexGrow: days,
       }}
     >
-      <p
-        style={{
-          color: selected ? "white" : "black",
-        }}
-      >
-        {days === 1 ? from.getDate() : `${from.getDate()} - ${to.getDate()}`}
-      </p>
-      {selected && (
-        <svg
-          className="absolute right-1 ml-1"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-        >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20ZM10 14.17L16.59 7.58002L18 9.00002L10 17L6 13L7.41 11.59L10 14.17Z"
-            fill="white"
-          />
-        </svg>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <p
+            style={{
+              color: selected ? "white" : "black",
+            }}
+          >
+            {days === 1
+              ? from.getDate()
+              : `${from.getDate()} - ${to.getDate()}`}
+          </p>
+          {selected && (
+            <svg
+              className="absolute right-1 ml-1"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20ZM10 14.17L16.59 7.58002L18 9.00002L10 17L6 13L7.41 11.59L10 14.17Z"
+                fill="white"
+              />
+            </svg>
+          )}
+        </>
       )}
     </button>
   );
--- kabinizer-front-end/src/pages/selectPeriods/Calendar/DrawPeriodsCalendar.tsx
+++ kabinizer-front-end/src/pages/selectPeriods/Calendar/DrawPeriodsCalendar.tsx
@@ -7,35 +7,69 @@ import { CompareDates } from "@/utils";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import useUser from "@/hooks/useUser.tsx";
 import { getMonthsMap, GetWeeklyPeriods } from "@/utils/calendar.ts";
-import Weeks from "@/pages/selectPeriods/Calendar/Weeks.tsx";
-import { MonthsContainer } from "@/pages/selectPeriods/Calendar/MonthsContainer.tsx";
+import WeekNumber from "@/components/WeekNumber.tsx";
+import MonthColumn from "@/components/MonthVertical.tsx";
+import { COLORS, MONTHS } from "@/options";
+import OptionButton from "@/components/OptionButton.tsx";
+import { useState } from "react";
 
 const DrawPeriodsCalendar = () => {
   const user = useUser();
   const queryClient = useQueryClient();
 
-  const { data: draws, isLoading: isLoadingDraws } = useQuery(
-    "getApiDraw",
-    DrawService.getApiDraw,
+  const {
+    data: draws,
+    isLoading: isLoadingDraws,
+    error: drawError,
+  } = useQuery("getApiDraw", DrawService.getApiDraw);
+  const {
+    data: bookingRequests,
+    isLoading: isLoadingBookingRequests,
+    error: bookingError,
+  } = useQuery(
+    "myBookingRequests",
+    () => BookingRequestService.getApiBookingRequestUser(user.localAccountId),
+    { enabled: !!user.localAccountId },
   );
-  const { data: bookingRequests, isLoading: isLoadingBookingRequests } =
-    useQuery(
-      "myBookingRequests",
-      () => BookingRequestService.getApiBookingRequestUser(user.localAccountId),
-      { enabled: !!user.localAccountId },
-    );
-
+  const [loadingStates, setLoadingStates] = useState<Record<string, boolean>>({
+    "": false,
+  });
   const addBooking = useMutation(
     "postApiBooking",
-    (payload: CreateBookingRequestDto[]) =>
-      BookingRequestService.postApiBookingRequest(payload),
-    { onSuccess: () => queryClient.invalidateQueries("myBookingRequests") },
+    (payload: CreateBookingRequestDto[]) => {
+      payload.forEach((p) => {
+        setLoadingStates((prev) => ({ ...prev, [p.periodId || ""]: true }));
+      });
+      return BookingRequestService.postApiBookingRequest(payload);
+    },
+    {
+      onSettled: (_data, _error, variables) => {
+        variables.forEach((bookingRequestDto) => {
+          setLoadingStates((prev) => ({
+            ...prev,
+            [bookingRequestDto.periodId || ""]: false,
+          }));
+        });
+        return queryClient.invalidateQueries("myBookingRequests");
+      },
+    },
   );
   const removeBooking = useMutation(
     "deleteApiBooking",
-    (payload: string[]) =>
-      BookingRequestService.deleteApiBookingRequest(payload),
-    { onSuccess: () => queryClient.invalidateQueries("myBookingRequests") },
+    (payload: string[]) => {
+      payload.forEach((id) => {
+        setLoadingStates((prev) => ({ ...prev, [id]: true }));
+      });
+      return BookingRequestService.deleteApiBookingRequest(payload);
+    },
+    {
+      onSettled: (_data, _error, variables) => {
+        variables.forEach((id) => {
+          setLoadingStates((prev) => ({ ...prev, [id]: false }));
+        });
+        return queryClient.invalidateQueries("myBookingRequests");
+      },
+    },
   );
 
   if (isLoadingDraws || isLoadingBookingRequests) return <div>Loading...</div>;
@@ -43,6 +77,14 @@ const DrawPeriodsCalendar = () => {
   const periods = draws
     ?.flatMap((value) => value.periods ?? [])
     .sort(CompareDates);
+
+  if (drawError || bookingError) {
+    return (
+      <div>
+        <p>An error occurred while fetching data. Please try again later.</p>
+      </div>
+    );
+  }
 
   if (!periods?.length) return <div>No periods</div>;
 
@@ -58,14 +100,50 @@ const DrawPeriodsCalendar = () => {
   return (
     <div className="flex flex-col gap-1">
       {Object.entries(monthMap).map(([month, weeks]) => (
-        <MonthsContainer key={month} month={Number(month)}>
-          <Weeks
-            weeks={weeks}
-            selected={bookingRequests || []}
-            month={Number(month)}
-            onClick={toggleBooking}
+        <div key={month} className="flex items-center gap-x-4">
+          <MonthColumn
+            month={MONTHS[Number(month)]}
+            color={COLORS[Number(month) % COLORS.length].selected}
           />
-        </MonthsContainer>
+          <div
+            className={`relative flex w-72 flex-col rounded-lg p-2 ${
+              Number(month) % 2
+                ? "bg-blue-200 dark:bg-blue-700"
+                : "bg-purple-200 dark:bg-purple-700"
+            }`}
+          >
+            <div className="flex h-full flex-col gap-y-4">
+              {Object.entries(weeks).map(([week, options]) => (
+                <div key={week} className="flex w-full gap-x-4">
+                  <WeekNumber value={Number(week)} />
+                  <div className="grid h-[30px] w-full grid-cols-7 gap-x-[9px]">
+                    {options.map((option) => (
+                      <div
+                        key={option.id}
+                        className={`col-start-${option.start} col-span-${option.end - option.start + 1}`}
+                      >
+                        <OptionButton
+                          key={option.id}
+                          colors={COLORS[Number(month) % 2]}
+                          from={option.from}
+                          to={option.to}
+                          isSpecial={option.isSpecial}
+                          selected={
+                            !!bookingRequests?.find(
+                              (s) => s.periodId === option.id,
+                            )
+                          }
+                          onClick={() => toggleBooking(option.id ?? "")}
+                          isLoading={loadingStates[option.id || ""]}
+                        />
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       ))}
     </div>
   );
