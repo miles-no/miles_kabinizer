4564b545fc6ce2e9cea56e79ede98fa1d1833507
Henry Sjøen <henry.sjoen@miles.no>
2024-04-22 11:29:19 +0200

    generate api

--- kabinizer-front-end/api/services/DrawService.ts
+++ kabinizer-front-end/api/services/DrawService.ts
@@ -4,6 +4,7 @@
 /* eslint-disable */
 import type { CreateDrawDto } from "../models/CreateDrawDto";
 import type { Draw } from "../models/Draw";
+import type { DrawEntity } from "../models/DrawEntity";
 
 import type { CancelablePromise } from "../core/CancelablePromise";
 import { OpenAPI } from "../core/OpenAPI";
@@ -23,17 +24,80 @@ export class DrawService {
 
   /**
    * @param requestBody
-   * @returns any Success
+   * @returns DrawEntity Success
    * @throws ApiError
    */
   public static postApiDraw(
     requestBody: CreateDrawDto,
-  ): CancelablePromise<any> {
+  ): CancelablePromise<DrawEntity> {
     return __request(OpenAPI, {
       method: "POST",
       url: "/api/Draw",
       body: requestBody,
       mediaType: "application/json",
+    });
+  }
+
+  /**
+   * @returns Draw Success
+   * @throws ApiError
+   */
+  public static getApiDrawCurrent(): CancelablePromise<Array<Draw>> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/api/Draw/current",
+    });
+  }
+
+  /**
+   * @returns Draw Success
+   * @throws ApiError
+   */
+  public static getApiDrawUpcoming(): CancelablePromise<Array<Draw>> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/api/Draw/upcoming",
+    });
+  }
+
+  /**
+   * @returns Draw Success
+   * @throws ApiError
+   */
+  public static getApiDrawPast(): CancelablePromise<Array<Draw>> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/api/Draw/past",
+    });
+  }
+
+  /**
+   * @param drawId
+   * @returns DrawEntity Success
+   * @throws ApiError
+   */
+  public static getApiDraw1(drawId: string): CancelablePromise<DrawEntity> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/api/Draw/{drawId}",
+      path: {
+        drawId: drawId,
+      },
+    });
+  }
+
+  /**
+   * @param drawId
+   * @returns any Success
+   * @throws ApiError
+   */
+  public static deleteApiDraw(drawId: string): CancelablePromise<any> {
+    return __request(OpenAPI, {
+      method: "DELETE",
+      url: "/api/Draw/{drawId}",
+      path: {
+        drawId: drawId,
+      },
     });
   }
 }
--- kabinizer-front-end/api/services/PeriodService.ts
+++ kabinizer-front-end/api/services/PeriodService.ts
@@ -3,6 +3,7 @@
 /* tslint:disable */
 /* eslint-disable */
 import type { Period } from "../models/Period";
+import type { ReadPeriodDto } from "../models/ReadPeriodDto";
 
 import type { CancelablePromise } from "../core/CancelablePromise";
 import { OpenAPI } from "../core/OpenAPI";
@@ -10,13 +11,78 @@ import { request as __request } from "../core/request";
 
 export class PeriodService {
   /**
+   * @param periodId
+   * @returns ReadPeriodDto Success
+   * @throws ApiError
+   */
+  public static getApiPeriod(
+    periodId: string,
+  ): CancelablePromise<ReadPeriodDto> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/api/Period/{periodId}",
+      path: {
+        periodId: periodId,
+      },
+      errors: {
+        404: `Not Found`,
+      },
+    });
+  }
+
+  /**
    * @returns Period Success
    * @throws ApiError
    */
-  public static getApiPeriod(): CancelablePromise<Array<Period>> {
+  public static getApiPeriod1(): CancelablePromise<Array<Period>> {
     return __request(OpenAPI, {
       method: "GET",
       url: "/api/Period",
+      errors: {
+        404: `Not Found`,
+      },
+    });
+  }
+
+  /**
+   * @returns Period Success
+   * @throws ApiError
+   */
+  public static getApiPeriodUpcoming(): CancelablePromise<Array<Period>> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/api/Period/upcoming",
+      errors: {
+        404: `Not Found`,
+      },
+    });
+  }
+
+  /**
+   * @returns Period Success
+   * @throws ApiError
+   */
+  public static getApiPeriodPast(): CancelablePromise<Array<Period>> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/api/Period/past",
+      errors: {
+        404: `Not Found`,
+      },
+    });
+  }
+
+  /**
+   * @returns Period Success
+   * @throws ApiError
+   */
+  public static getApiPeriodAll(): CancelablePromise<Array<Period>> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/api/Period/all",
+      errors: {
+        404: `Not Found`,
+      },
     });
   }
 }
